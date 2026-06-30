d64aa180bc57c66b5331b8c0f149d2289f8d0717
mohammed_ahsraf <mohammedashraf20062006@gmail.com>
2026-06-30 06:14:12 +0300

    solve UI bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Advantages from "@/components/advantages/Advantages";
 import CardDetails from "@/components/card_details/CardDetails";
@@ -7,7 +7,6 @@ import Description from "@/components/description/Description";
 import Footer from "@/components/footer/Footer";
 import Header from "@/components/header/Header";
 import PartenerShip from "@/components/partenership/PartenerShip";
-import Statistics from "@/components/statistics/Statistics";
 import Try from "@/components/try/Try";
 import { useEffect } from "react";
 
@@ -16,11 +15,10 @@ function Home() {
     if ("scrollRestoration" in history) {
       history.scrollRestoration = "manual";
     }
-  }, [])
+  }, []);
   return (
     <div>
       <Header />
-      <Statistics />
       <Description />
       <Advantages />
       <CardDetails />
